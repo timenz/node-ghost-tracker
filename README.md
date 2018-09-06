7717b7558c7dbad89d0d37ec8d285543550ec135
bang Tim <timenmail@gmail.com>
2018-09-06 14:46:29 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Node Ghost Tracker
+
+Simple node app to provide api to search data in posts for [ghost blog app](https://github.com/tryghost/ghost)
+
+## Requirement
+- ghost
+- mysql backend
+
+## Install
+
+
+## Notes
+
+This is just initial app, there is many part that need improvement
---
+++ routes/index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var router = express.Router();
+var debug = require('debug')('app:tracker');
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB
+});
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.json({ name: 'node-ghost-tracker', version: '1.0.0' });
+});
+
+const resultCount = (query) => {
+  return new Promise((resolve, reject) => {
+    connection.query({
+      sql: 'SELECT COUNT(`title`) FROM `posts` WHERE `plaintext` regexp ?',
+      timeout: 40000, // 40s
+      values: [query]
+    }, function (error, results, fields) {
+      if (error) return reject(error)
+      resolve(results)
+    });
+  })
+}
+
+const resultData = (query, page, limit) => {
+  return new Promise((resolve, reject) => {
+    connection.query({
+      sql: 'SELECT `title`, `slug`, `plaintext` FROM `posts` WHERE `plaintext` regexp ? limit ?,?',
+      timeout: 40000, // 40s
+      values: [query, page, limit]
+    }, function (error, results, fields) {
+      if (error) return reject(error)
+      resolve(results)
+    });
+  })
+}
+
+router.get('/search', function(req, res, next) {
+  const query = req.query.q || null
+  const page = req.query.p || 0
+  const limit = req.query.l || 30
+  resultCount(query)
+    .then(result => {
+      if (result.length > 0) return resultData(query, page, limit)
+    })
+    .then(result => {
+      res.json(result)
+    }).catch(err => {
+      next(err)
+    })
+});
+
+module.exports = router;
